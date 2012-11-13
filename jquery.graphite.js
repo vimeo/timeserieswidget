c1c19e7e4c7ded26d7a91e82959dbc84685a9743
Dieter Plaetinck <dieter@plaetinck.be>
2012-11-12 22:29:59 -0500

    add rickshaw support

--- jquery.graphite.js
+++ jquery.graphite.js
@@ -9,7 +9,11 @@ function build_url(options) {
     $.each(options, function (key, value) {
         if (key === "target") {
             $.each(value, function (index, value) {
-                url += "&target=" + value;
+                if (value.data) { // rickshaw's "target is a dictionary"
+                    url += "&target=" + value.data;
+                } else { // png's "target is a string
+                    url += "&target=" + value;
+                }
             });
         } else if (value !== null && key !== "url") {
             url += "&" + key + "=" + value;
@@ -55,6 +59,80 @@ function build_url(options) {
             $.fn.graphite.render($this, settings);
         });
     };
+
+    $.fn.graphiteRick = function (options) {
+        options = options || {};
+        var settings = $.extend({}, $.fn.graphite.defaults, options);
+
+        return this.each(function () {
+            $this = $(this);
+
+            $this.data("graphOptions", settings);
+            $.fn.graphiteRick.render(this, settings);
+        });
+    };
+
+    $.fn.graphiteRick.render = function(div, options, on_error) {
+        $div = $(div);
+        $div.attr("height", options.height);
+        $div.attr("width", options.width);
+        var drawRick = function(response) {
+            if (response.length != options.target.length) {
+                error = "num requested targets: " + options.target.length +
+                        ", but # targets for which we received data: " + response.length;
+                return or_error (error);
+            }
+            var all_targets = [];
+            for (var t = 0; t < response.length; t++) {
+                // create target objects from config we had, but the received datapoints merged in
+                var target = options.target[t];
+                target.data = [];
+                for (var i in response[t].datapoints) {
+                    target.data[i] = { x: response[t].datapoints[i][1], y: response[t].datapoints[i][0] || 0 };
+                }
+                all_targets.push(target);
+            }
+            var graph = new Rickshaw.Graph({
+                element: div,
+                height: options.height,
+                width: options.width,
+                series: all_targets
+            });
+            if(options['x_axis']) {
+                var x_axis = new Rickshaw.Graph.Axis.Time( { graph: graph } );
+            }
+            if(options['y_axis']) {
+                var y_axis = new Rickshaw.Graph.Axis.Y( {
+                    graph: graph,
+                    orientation: 'left',
+                    tickFormat: Rickshaw.Fixtures.Number.formatKMBT,
+                    element: document.getElementById(options['y_axis']),
+                });
+            }
+            graph.render();
+            if (options['legend']) {
+                var legend = new Rickshaw.Graph.Legend({
+                    graph: graph,
+                    element: document.getElementById(options['legend'])
+                });
+                if(options['legend.toggle']) {
+                    var shelving = new Rickshaw.Graph.Behavior.Series.Toggle({
+                        graph: graph,
+                        legend: legend
+                    });
+                }
+            }
+          }
+     $.ajax({
+        accepts: {text: 'application/json'},
+        cache: false,
+        dataType: 'jsonp',
+        jsonp: 'jsonp',
+        url: build_url(options) + '&format=json',
+        error: function(xhr, textStatus, errorThrown) { on_error(textStatus + ": " + errorThrown); }
+      }).done(drawRick);
+    };
+
 
     // Default settings. 
     // Override with the options argument for per-case setup
