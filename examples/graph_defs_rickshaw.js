886b3d74050a60d790897803306e605bd55bbf75
Dieter Plaetinck <dieter@plaetinck.be>
2013-04-09 21:23:31 -0400

    update examples for new statsd histogram paths

--- examples/graph_defs_rickshaw.js
+++ examples/graph_defs_rickshaw.js
@@ -35,39 +35,39 @@ graph_rickshaw_fancy_stacked = {
     targets: [
         {name: '0.01',
          color: '2FFF00',
-         target: "scale(divideSeries(stats.timers.render_time.bin_0_01,stats.timers.render_time.count),0.1)"
+         target: "scale(divideSeries(stats.timers.render_time.histogram.bin_0.01,stats.timers.render_time.count),0.1)"
         },
         {name: '0.05',
          color: '64DD0E',
-         target: "scale(divideSeries(stats.timers.render_time.bin_0_05,stats.timers.render_time.count),0.1)"
+         target: "scale(divideSeries(stats.timers.render_time.histogram.bin_0.05,stats.timers.render_time.count),0.1)"
         },
         {name: '0.1',
          color: '9CDD0E',
-         target: "scale(divideSeries(stats.timers.render_time.bin_0_1,stats.timers.render_time.count),0.1)"
+         target: "scale(divideSeries(stats.timers.render_time.histogram.bin_0.1,stats.timers.render_time.count),0.1)"
         },
         {name: '0.5',
          color: 'DDCC0E',
-         target: "scale(divideSeries(stats.timers.render_time.bin_0_5,stats.timers.render_time.count),0.4)"
+         target: "scale(divideSeries(stats.timers.render_time.histogram.bin_0.5,stats.timers.render_time.count),0.4)"
         },
         {name: '1',
          color: 'DDB70E',
-         target: "scale(divideSeries(stats.timers.render_time.bin_1,stats.timers.render_time.count),0.5)"
+         target: "scale(divideSeries(stats.timers.render_time.histogram.bin_1,stats.timers.render_time.count),0.5)"
         },
         {name: '5',
          color: 'FF6200',
-         target: "scale(divideSeries(stats.timers.render_time.bin_5,stats.timers.render_time.count),4.0)"
+         target: "scale(divideSeries(stats.timers.render_time.histogram.bin_5,stats.timers.render_time.count),4.0)"
         },
         {name: '10',
          color: 'FF3C00',
-         target: "scale(divideSeries(stats.timers.render_time.bin_10,stats.timers.render_time.count),5.0)"
+         target: "scale(divideSeries(stats.timers.render_time.histogram.bin_10,stats.timers.render_time.count),5.0)"
         },
         {name: '50',
          color: 'FF1E00',
-         target: "scale(divideSeries(stats.timers.render_time.bin_50,stats.timers.render_time.count),40.0)"
+         target: "scale(divideSeries(stats.timers.render_time.histogram.bin_50,stats.timers.render_time.count),40.0)"
         },
         {name: 'inf',
          color: 'FF0000',
-         target: "scale(divideSeries(stats.timers.render_time.bin_inf,stats.timers.render_time.count),60.0)"
+         target: "scale(divideSeries(stats.timers.render_time.histogram.bin_inf,stats.timers.render_time.count),60.0)"
         }
     ],
     title: "render time histogram",
